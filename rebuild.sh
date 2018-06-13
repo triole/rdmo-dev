1fd21c1fed5ce4256d94e0c6aa6912e4db130e75
Olaf Michaelis <omichaelis@aip.de>
2018-06-13 09:46:19 +0200

    Add rdmo using python2

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [[ "${1}" == "p2" ]]; then
+    echo -e "\nBuilding python2 version...\n"
+    cp -f ./rdmo/dockerfile.p2 ./rdmo/dockerfile
+else
+    echo "-e \nBuilding python3 version...\n"
+    cp -f ./rdmo/dockerfile.p3 ./rdmo/dockerfile
+fi
+
 sudo docker-compose down --rmi all --remove-orphans
 sudo docker volume rm \
     rdmo-dc_rdmosrc \
     rdmo-dc_volfiles
 
 sudo docker-compose up -d
-sudo docker-compose logs -f 
+sudo docker-compose logs -f
