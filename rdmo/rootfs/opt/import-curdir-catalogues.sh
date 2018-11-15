7fc37ac64c8b2c7dd0e678c951a492bfd0635ed3
Olaf Michaelis <omichaelis@aip.de>
2018-11-15 12:18:45 +0100

    Add user switch for catalog import

--- rdmo/rootfs/opt/import-curdir-catalogues.sh
+++ rdmo/rootfs/opt/import-curdir-catalogues.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ -n "${1}" ]]; then
+    user="--user ${1}"
+else
+    user=""
+fi
+
 curdir="$(pwd)"
 cd ${RDMO_APP}
 
@@ -11,5 +17,5 @@ unset IFS
 
 for i in "${sorted[@]}"; do
    echo -e "Starting to import ${i}..."
-   python manage.py import ${i}
+   python manage.py import "${i}" ${user}
 done
--- rdmo/rootfs/opt/import-github-catalogues.sh
+++ rdmo/rootfs/opt/import-github-catalogues.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [[ -n "${1}" ]]; then
+    user="--user ${1}"
+else
+    user=""
+fi
+
 cd "${SRC}"
 git clone "https://github.com/rdmorganiser/rdmo-catalog" "rdmo-catalog"
-
 
 cd ${RDMO_APP}
 
@@ -17,5 +22,5 @@ unset IFS
 
 for i in "${sorted[@]}"; do
    echo -e "Starting to import ${i}..."
-   python manage.py import ${i}
+   python manage.py import "${i}" ${user}
 done
