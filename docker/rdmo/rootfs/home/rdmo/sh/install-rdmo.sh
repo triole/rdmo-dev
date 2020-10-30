685611cf94febd064bcb30ab657a2d186f1ebe80
olaf michaelis <o.mic@web.de>
2020-10-30 13:06:25 +0100

    Make installation more verbose

--- docker/rdmo/rootfs/home/rdmo/sh/install-rdmo.sh
+++ docker/rdmo/rootfs/home/rdmo/sh/install-rdmo.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 mkdir -p "${RDMO_SOURCE_MP}"
-
 cd "${RDMO_SOURCE_MP}"
-if [[ "${RDMO_SOURCE_MP}" != "${INSTALL_SOURCE}" ]]; then
+
+function ec() {
+    echo -e "\n\033[0;93m${1}\033[0m"
+    eval "${1}"
+}
+
+if [[ ("${RDMO_SOURCE_MP}" != "${INSTALL_SOURCE}") && -n "${INSTALL_SOURCE}" ]]; then
     echo "Install rdmo from: ${INSTALL_SOURCE}"
-    git clone "${INSTALL_SOURCE}" .
-    git checkout "${BRANCH}"
-    git pull
+    ec "git clone \"${INSTALL_SOURCE}\" ."
+    ec "git checkout \"${BRANCH}\""
+    ec "git pull"
 fi
-pip install -e "${RDMO_SOURCE_MP}"
+ec "pip install -e \"${RDMO_SOURCE_MP}\""
 
 # git clone https://github.com/rdmorganiser/rdmo-app ${RDMO_APP_MP}
 mkdir -p "${RDMO_SOURCE_MP}/testing/log"
@@ -22,8 +27,8 @@ cp -f \
     "${RDMO_SOURCE_MP}/testing/config/settings/local.py"
 
 cd "${RDMO_APP_MP}"
-python manage.py makemigrations
-python manage.py migrate
-python manage.py download_vendor_files
-python manage.py collectstatic --no-input
-python manage.py create_admin_user
+ec "python manage.py makemigrations"
+ec "python manage.py migrate"
+ec "python manage.py download_vendor_files"
+ec "python manage.py collectstatic --no-input"
+ec "python manage.py create_admin_user"
