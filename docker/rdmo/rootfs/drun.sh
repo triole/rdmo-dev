9cdbd612a57fba7775c882b30b03c0cd64e73160
olaf michaelis <o.mic@web.de>
2020-11-12 11:46:59 +0100

    Add wait for pg func to drun

--- docker/rdmo/rootfs/drun.sh
+++ docker/rdmo/rootfs/drun.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function waitforpg() {
+    pg_isready -h ${POSTGRES_HOST} -U ${POSTGRES_USER} || (
+        sleep 1
+        waitforpg
+    )
+}
+
+waitforpg
+
 if [[ -z "$(pip freeze | grep "rdmo")" ]]; then
     ${HOME}/sh/install-rdmo.sh
 fi
