8f01a190076a98ed1d7317f822c7f41e5b7955a4
olaf michaelis <o.mic@web.de>
2021-01-21 12:17:14 +0100

    Fix pgtool add tool volumes to postgres

--- docker/rdmo/rootfs/home/rdmo/sh/pg_add-api-key.sh
+++ docker/rdmo/rootfs/home/rdmo/sh/pg_add-api-key.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} \
+PGPASSWORD=admin \
+    psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} \
     -c "INSERT INTO authtoken_token (key, created, user_id) VALUES ('${API_DEV_KEY}', 'NOW', 1)"
